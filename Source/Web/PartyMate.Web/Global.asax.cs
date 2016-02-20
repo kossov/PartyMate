3727cb82d8cc6b908bd88103dc0638820bc4f016
Ognyan Kossov <ogi_2112@yahoo.com>
2016-02-21 01:40:45 +0200

    fixed db initializer

--- Source/Web/PartyMate.Web/Global.asax.cs
+++ Source/Web/PartyMate.Web/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿namespace PartyMate.Web
 {
+    using System.Data.Entity;
+
     using System.Reflection;
 
     using System.Web.Http;
@@ -18,7 +20,7 @@
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new RazorViewEngine());
 
-            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
             AutofacConfig.RegisterAutofac();
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
