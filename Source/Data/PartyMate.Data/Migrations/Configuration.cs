b2cd1749b7d015ab126a93e421124f11c8c321c0
Ognyan Kossov <ogi_2112@yahoo.com>
2016-02-23 08:00:32 +0200

    seeding more data

--- Source/Data/PartyMate.Data/Migrations/Configuration.cs
+++ Source/Data/PartyMate.Data/Migrations/Configuration.cs
@@ -8,8 +8,9 @@
 
     using Models;
 
-    using PartyMate.Common;
     using Common;
+    using PartyMate.Common;
+
     public sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
         public Configuration()
@@ -50,14 +51,13 @@
                 userManager.AddToRole(admin.Id, GlobalConstants.AdministratorRoleName);
 
                 // Add TelerikAcademy for Demo Purposes
-                var db = new ApplicationDbContext();
                 var location = new Location()
                 {
                     Longitude = 23.3772423,
                     Latitude = 42.6508509
                 };
 
-                var locations = new DeletableEntityRepository<Location>(db);
+                var locations = new DeletableEntityRepository<Location>(context);
                 locations.Add(location);
                 locations.SaveChanges();
 
@@ -72,12 +72,50 @@
                     FacebookUrl = "https://www.facebook.com/TelerikAcademy",
                     TwitterUrl = "https://twitter.com/TelerikAcademy",
                     Email = "",
-                    Capacity = 5000,
+                    Capacity = 2000,
                     ModeratorId = admin.Id
                 };
 
-                var clubs = new DeletableEntityRepository<Club>(db);
+                var review = new ClubReview()
+                {
+                    Club = club,
+                    Content = "TEST TEST TEST TEST",
+                    Rating = 5,
+                    Author = admin
+                };
+
+                var reviews = new DeletableEntityRepository<ClubReview>(context);
+                reviews.Add(review);
+                var clubs = new DeletableEntityRepository<Club>(context);
                 clubs.Add(club);
+
+                var club2 = new Club()
+                {
+                    ProfilePicUrl = "http://yaltaclub.com/images/yalta-logo.png?1383083648",
+                    Name = "Yalta Club",
+                    Adress = @"20, Tsar Osvoboditel Blvd, Sofia 1000, Bulgaria",
+                    Phone = "0897 870 230",
+                    Location = location,
+                    SiteUrl = "http://yaltaclub.com/",
+                    FacebookUrl = "https://www.facebook.com/yaltaclub",
+                    TwitterUrl = "",
+                    Email = "",
+                    Capacity = 4000,
+                    ModeratorId = admin.Id
+                };
+
+                var review2 = new ClubReview()
+                {
+                    Club = club2,
+                    Content = "TE ST TE ST TE ST",
+                    Author = admin,
+                    Rating = 4
+                };
+
+                reviews.Add(review2);
+
+                clubs.Add(club2);
+
                 clubs.SaveChanges();
             }
         }
